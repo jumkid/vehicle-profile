a3ac7d665f4c0b34b63afc8d74cd3bbd5f1e44dd
chooli <chooli.yip@gmail.com>
2022-09-23 16:07:46 -0400

    changed security configuration

--- src/main/java/com/jumkid/vehicle/config/SecurityConfig.java
+++ src/main/java/com/jumkid/vehicle/config/SecurityConfig.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -16,7 +15,6 @@ import org.springframework.web.client.RestTemplate;
 
 @Configuration
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true, jsr250Enabled = true)
 public class SecurityConfig {
 
     @Value("${jwt.token.enable}")
@@ -34,21 +32,14 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .antMatchers("/v2/api-docs",
-                        "/configuration/ui",
-                        "/swagger-resources/**",
-                        "/configuration/security",
-                        "/swagger-ui.html",
-                        "/webjars/**").permitAll() //whitelist
-                .antMatchers(enableTokenCheck? "/**" : "/admin-console").authenticated() //any other request
-                .and().sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+        http.antMatcher("/**")
+                .authorizeRequests().anyRequest().authenticated()
                 .and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .addFilterBefore(new BearerTokenRequestFilter(enableTokenCheck, tokenIntrospectUrl, restTemplate),
+                        UsernamePasswordAuthenticationFilter.class)
                 .csrf().disable();  // enable this if the authorization service exposure to public
-
-        http.addFilterBefore(new BearerTokenRequestFilter(enableTokenCheck, tokenIntrospectUrl, restTemplate),
-                UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
